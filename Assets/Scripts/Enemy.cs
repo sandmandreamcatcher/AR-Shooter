7b4b1ee88d8f5a6d766538c649b37726a7b563ea
sandmandreamcatcher <dr.catcher@ukr.net>
2021-07-29 14:07:22 +0300

    Initial commit

---
+++ Assets/Scripts/Enemy.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    [SerializeField] private ParticleSystem _deathEffect;
+
+    public void Die()
+    {
+        Instantiate(_deathEffect, transform.position, Quaternion.identity);
+        Destroy(gameObject);
+    }
+}
---
+++ Assets/Scripts/Spawner.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using UnityEngine;
+using Random = UnityEngine.Random;
+
+public class Spawner : MonoBehaviour
+{
+    [SerializeField] private Player _target;
+    [SerializeField] private float _spawnRadius;
+    [SerializeField] private float _secondsBetweenSpawn;
+    [SerializeField] private Enemy[] _enemies = new Enemy[]{};
+
+    private void Start()
+    {
+        StartCoroutine(SpawnRandomEnemy());
+    }
+
+    private IEnumerator SpawnRandomEnemy()
+    {
+        while (true)
+        {
+            Enemy newEnemy = Instantiate(_enemies[Random.Range(0, _enemies.Length)], RandomPlaceInSphere(_spawnRadius), Quaternion.identity);
+            Vector3 lookDirection = _target.transform.position - newEnemy.transform.position;
+            newEnemy.transform.rotation = Quaternion.LookRotation(lookDirection);
+
+            yield return new WaitForSeconds(_secondsBetweenSpawn);
+        }
+    }
+
+    private Vector3 RandomPlaceInSphere(float radius)
+    {
+        return Random.insideUnitSphere * radius;
+    }
+}
