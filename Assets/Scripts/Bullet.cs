54279d0dc97fd9c422a69c01068512bc38fa4582
sandmandreamcatcher <dr.catcher@ukr.net>
2021-07-29 15:31:08 +0300

    EnemyDestroy added

---
+++ Assets/Scripts/Bullet.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+public class Bullet : MonoBehaviour
+{
+    [SerializeField] private float _speed;
+
+    private void Update()
+    {
+        transform.Translate(Vector3.forward * _speed * Time.deltaTime);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.TryGetComponent(out Enemy enemy))
+        {
+            enemy.Die();
+            Destroy(gameObject);
+        }
+    }
+}
---
+++ Assets/Scripts/Shooter.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+
+public class Shooter : MonoBehaviour
+{
+    [SerializeField] private Transform _shootPoint;
+    [SerializeField] private Bullet _bulletTemplate;
+
+    private void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Instantiate(_bulletTemplate, _shootPoint);
+        }
+    }
+}
